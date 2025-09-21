68ec567a3092271e77f0b062aa0c9a756ca20a42
MartianK3jC <kceniza24@gmail.com>
2025-09-21 19:31:22 +0800

    Loading Image implementation done

--- ImageProcessingActivity/Form1.Designer.cs
+++ ImageProcessingActivity/Form1.Designer.cs
@@ -28,13 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.originalPicBox = new System.Windows.Forms.PictureBox();
+            this.editedPicBox = new System.Windows.Forms.PictureBox();
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.copyBtn = new System.Windows.Forms.Button();
+            this.grayBtn = new System.Windows.Forms.Button();
+            this.invertBtn = new System.Windows.Forms.Button();
+            this.histogramBtn = new System.Windows.Forms.Button();
+            this.sepiaBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.originalPicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.editedPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // originalPicBox
+            // 
+            this.originalPicBox.Location = new System.Drawing.Point(12, 203);
+            this.originalPicBox.Name = "originalPicBox";
+            this.originalPicBox.Size = new System.Drawing.Size(654, 462);
+            this.originalPicBox.TabIndex = 0;
+            this.originalPicBox.TabStop = false;
+            // 
+            // editedPicBox
+            // 
+            this.editedPicBox.Location = new System.Drawing.Point(708, 203);
+            this.editedPicBox.Name = "editedPicBox";
+            this.editedPicBox.Size = new System.Drawing.Size(654, 462);
+            this.editedPicBox.TabIndex = 1;
+            this.editedPicBox.TabStop = false;
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.Location = new System.Drawing.Point(29, 38);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(175, 54);
+            this.loadBtn.TabIndex = 2;
+            this.loadBtn.Text = "Load image";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(231, 38);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(175, 54);
+            this.saveBtn.TabIndex = 3;
+            this.saveBtn.Text = "Save image";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            // 
+            // copyBtn
+            // 
+            this.copyBtn.Location = new System.Drawing.Point(29, 119);
+            this.copyBtn.Name = "copyBtn";
+            this.copyBtn.Size = new System.Drawing.Size(175, 54);
+            this.copyBtn.TabIndex = 4;
+            this.copyBtn.Text = "Basic Copy";
+            this.copyBtn.UseVisualStyleBackColor = true;
+            // 
+            // grayBtn
+            // 
+            this.grayBtn.Location = new System.Drawing.Point(231, 119);
+            this.grayBtn.Name = "grayBtn";
+            this.grayBtn.Size = new System.Drawing.Size(175, 54);
+            this.grayBtn.TabIndex = 5;
+            this.grayBtn.Text = "Grayscale";
+            this.grayBtn.UseVisualStyleBackColor = true;
+            // 
+            // invertBtn
+            // 
+            this.invertBtn.Location = new System.Drawing.Point(435, 119);
+            this.invertBtn.Name = "invertBtn";
+            this.invertBtn.Size = new System.Drawing.Size(175, 54);
+            this.invertBtn.TabIndex = 6;
+            this.invertBtn.Text = "Color Inversion";
+            this.invertBtn.UseVisualStyleBackColor = true;
+            // 
+            // histogramBtn
+            // 
+            this.histogramBtn.Location = new System.Drawing.Point(641, 119);
+            this.histogramBtn.Name = "histogramBtn";
+            this.histogramBtn.Size = new System.Drawing.Size(175, 54);
+            this.histogramBtn.TabIndex = 7;
+            this.histogramBtn.Text = "Histogram";
+            this.histogramBtn.UseVisualStyleBackColor = true;
+            // 
+            // sepiaBtn
+            // 
+            this.sepiaBtn.Location = new System.Drawing.Point(844, 119);
+            this.sepiaBtn.Name = "sepiaBtn";
+            this.sepiaBtn.Size = new System.Drawing.Size(175, 54);
+            this.sepiaBtn.TabIndex = 8;
+            this.sepiaBtn.Text = "Sepia";
+            this.sepiaBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1374, 715);
+            this.Controls.Add(this.sepiaBtn);
+            this.Controls.Add(this.histogramBtn);
+            this.Controls.Add(this.invertBtn);
+            this.Controls.Add(this.grayBtn);
+            this.Controls.Add(this.copyBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.loadBtn);
+            this.Controls.Add(this.editedPicBox);
+            this.Controls.Add(this.originalPicBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.originalPicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.editedPicBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox originalPicBox;
+        private System.Windows.Forms.PictureBox editedPicBox;
+        private System.Windows.Forms.Button loadBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button copyBtn;
+        private System.Windows.Forms.Button grayBtn;
+        private System.Windows.Forms.Button invertBtn;
+        private System.Windows.Forms.Button histogramBtn;
+        private System.Windows.Forms.Button sepiaBtn;
     }
 }
 
--- ImageProcessingActivity/Form1.cs
+++ ImageProcessingActivity/Form1.cs
@@ -16,5 +16,34 @@ namespace ImageProcessingActivity
         {
             InitializeComponent();
         }
+
+        //Loading an original picture to originalPicBox
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+            //Step 1: Create and configure the file dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog(); // -> creation of a windows file picker dialog
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif"; // -> restricts file types to common image formats. Format: Display Name|file patterns.... ex. name.png, etc chuchu
+            openFileDialog.Title = "Select an Image File";
+
+            //Step 2: Show the dialog and check if user selected a file
+            if(openFileDialog.ShowDialog() == DialogResult.OK)//-> this shows the dialog and checks if user clicks OK
+            {
+                try
+                {
+                    //Step 3: Load the Selected Image
+                    Image selectedImage = Image.FromFile(openFileDialog.FileName);
+
+                    //Step 4: Display it in the original picture box
+                    originalPicBox.Image = selectedImage; // -> assigns image to picture box
+                    originalPicBox.SizeMode = PictureBoxSizeMode.StretchImage; // -> StretchImage to make it fit the picture box
+                }
+                catch (Exception ex)
+                {
+                    //Step 5: Handle any errors (corrupted files, ect.)
+                    MessageBox.Show("Error loading image: " + ex.Message);
+                }
+            }
+
+        }
     }
 }
